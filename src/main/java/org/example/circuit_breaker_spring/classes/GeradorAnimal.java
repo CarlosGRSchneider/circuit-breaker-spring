877a7a47f0d82b113904433dafe1ebadea048c43
CarlosGuilherme <carlosguilherme.schneider@gmail.com>
2025-01-14 23:14:23 -0300

    Troca de nome de variavel

--- src/main/java/org/example/circuit_breaker_spring/classes/GeradorAnimal.java
+++ src/main/java/org/example/circuit_breaker_spring/classes/GeradorAnimal.java
@@ -11,7 +11,7 @@ public class GeradorAnimal {
                     alegre curioso tranquilo rápido brincalhão atento inteligente gentil elegante perplexo sonolento fiel astuto amigável paciente educado corajoso campeiro ambicioso ninja
                     """.strip().replace("\n", " ").split("\\s+")
     );
-    private static final List<String> animais = List.of(
+    private static final List<String> ANIMAIS = List.of(
             """
                     Caranguejo Quati Suricato Elefante Leão Tigre Urso Lobo Panda Canguru Golfinho Ratão-do-banhado Pica-pau Gavião Coala Quero-quero Sapo Cachorro Gato Grilo
                     """.strip().replace("\n", " ").split("\\s+")
@@ -19,7 +19,7 @@ public class GeradorAnimal {
 
     public static String geraCombinacao() {
 
-        String animal = animais.get(random.nextInt(animais.size()));
+        String animal = ANIMAIS.get(random.nextInt(ANIMAIS.size()));
         String adjetivo = ADJETIVOS.get(random.nextInt(ADJETIVOS.size()));
         return animal + " " + adjetivo;
     }
